00dcf4de03e55f0f8524e47238c7274cf3010fa4
HPow <hpowell84@hotmail.com>
2026-02-14 19:12:43 +0000

    loads json file

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-<script>
+
 
 
 const countryName = document.querySelector(".countryName");
 const population= document.querySelector(".population");
 const area = document.querySelector(".area");
 
+let randomNumber = Math.floor(Math.random() * 247) + 1;
+console.log(randomNumber);
+
 startButton = document.createElement("button");
   startButton.textContent = "Start";
   document.body.append(startButton);
   startButton.addEventListener("click", startGame);
+
+async function loadCountries() {
+	const res = await fetch("data/countryList.json);
+	const countries = await res.json();
+	
+	console.log("loaded");
+	
+	let randomIndex = Math.floor(Math.random() * countries.length);
+	let randomCountry = countries[randomIndex];
+
+	console.log(randomCountry);
+}
+
+loadCountries();
 
 
 
@@ -28,4 +45,3 @@ startButton = document.createElement("button");
   	country2Button.addEventListener("click", startGame);
 }
 	
-</script>
