e8d31138eb43cf1cd82ec9a9752d177cbd2095b5
HPow <hpowell84@hotmail.com>
2026-07-28 16:20:06 +0100

    Fixed Netlify function name

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ async function refreshCountries() {
 
 		let data1;
 		try{
-			const res1 = await fetch(`/.netlify/functions/country?code=${countryCode1}`, {cache: "no-store"});
+			const res1 = await fetch(`/.netlify/functions/countryAPI?code=${countryCode1}`, {cache: "no-store"});
 			if(!res1.ok) {
 				throw new Error("API error");
 			}
@@ -102,7 +102,7 @@ async function refreshCountries() {
 		let data2;
 
 		try{
-			const res2 = await fetch(`/.netlify/functions/country?code=${countryCode2}`, {cache: "no-store"});
+			const res2 = await fetch(`/.netlify/functions/countryAPI?code=${countryCode2}`, {cache: "no-store"});
 
 			if(!res2.ok) {
 				throw new Error("API error");
